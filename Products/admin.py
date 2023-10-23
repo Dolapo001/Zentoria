beb1077c2b180b406dde7fdc2095a86484e15ad4
Dolapo24 <adedolapovictoria927@gmail.com>
2023-10-23 21:00:45 +0100

    made commits on product model

--- Products/admin.py
+++ Products/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Category, Product, ProductAttribute, ProductReview
+
 
 # Register your models here.
+class SubcategoryInline(admin.TabularInline):
+    model = Category
+    fk_name = 'category'
+    extra = 1
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category']
+    list_filter = ['category']
+    search_fields = ['name']
+    inlines = [SubcategoryInline]
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'inventory', 'category']
+    list_filter = ['category']
+    search_fields = ['name', 'description']
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(ProductAttribute)
+admin.site.register(ProductReview)
--- Products/models.py
+++ Products/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='subcategory')
+    image = models.ImageField(upload_to='category_images/', null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product_images/')
+    sales_history = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductAttribute(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    value = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.product.name} - {self.name}: {self.value}"
+
+
+class ProductReview(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField()
+    review_text = models.TextField()
+    review_date = models.DateTimeField(auto_now_add=True)
+    review_image = models.ImageField(upload_to='review_images/', null=True, blank=True)
+
+    def __str__(self):
+        return f"Review for {self.product.name} by {self.user.username}"
+
+
+class FavouriteProduct(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Favourite: {self.product.name}"
