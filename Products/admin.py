5455784da1282a04794813143400e5e4c8d7f9cd
Dolapo24 <adedolapovictoria927@gmail.com>
2023-10-23 23:55:20 +0100

    modified product model

--- Products/admin.py
+++ Products/admin.py
@@ -1,28 +1,34 @@
 from django.contrib import admin
-from .models import Category, Product, ProductAttribute, ProductReview
+from .models import Category, Style, Product, ProductAttribute, ProductReview, SubCategory
+
 
 
 # Register your models here.
-class SubcategoryInline(admin.TabularInline):
-    model = Category
-    fk_name = 'category'
-    extra = 1
-
-
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'category']
-    list_filter = ['category']
+    list_display = ['name', 'parent_category', 'description']
+    list_filter = ['parent_category']
     search_fields = ['name']
-    inlines = [SubcategoryInline]
+
+
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'parent_category']
+    list_filter = ['parent_category']
+    search_fields = ['name']
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'price', 'inventory', 'category']
-    list_filter = ['category']
+    list_display = ['name', 'price', 'quantity', 'category']
+    list_filter = ['subcategory__name']
     search_fields = ['name', 'description']
 
 
+class StyleAdmin(admin.ModelAdmin):
+    list_display = ['style']
+
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(SubCategory, SubCategoryAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(ProductAttribute)
 admin.site.register(ProductReview)
+admin.site.register(Style, StyleAdmin)
--- Products/models.py
+++ Products/models.py
@@ -1,14 +1,39 @@
 from django.db import models
 from django.contrib.auth.models import User
+import random
+import string
 
 # Create your models here.
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    category = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='subcategory')
-    image = models.ImageField(upload_to='category_images/', null=True, blank=True)
+    icon = models.ImageField(upload_to='category_icons/', null=True, blank=True)
+    parent_category = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='subcategory')
     description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Style(models.Model):
+    style = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.style
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=100)
+    parent_category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='subcategories')
+
+    class Meta:
+        verbose_name = "Subcategory"
+        verbose_name_plural = "Subcategories"
 
     def __str__(self):
         return self.name
@@ -18,22 +43,40 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    inventory = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE, null=True)
     image = models.ImageField(upload_to='product_images/')
-    sales_history = models.PositiveIntegerField(default=0)
+    specification = models.CharField(max_length=100, blank=True, null=True)
+    size = models.CharField(max_length=100, blank=True, null=True)
+    color = models.CharField(max_length=100, blank=True, null=True)
+    style = models.ForeignKey(Style, on_delete=models.SET_NULL, null=True)
+    style_code = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.name
 
+    @property
+    def generate_style_code(self):
+        if self.style:
+            code_length = 10
+            alphanumeric_characters = string.ascii_letters + string.digits
+            return ''.join(random.choice(alphanumeric_characters) for _ in range(code_length))
+        return ""
+
+    def save(self, *args, **kwargs):
+        if not self.style_code:
+            self.style_code = self.generate_style_code
+        super().save(*args, **kwargs)
+
 
 class ProductAttribute(models.Model):
+    attribute_type = models.CharField(max_length=50)
+    attribute_value = models.CharField(max_length=100)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-    value = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.product.name} - {self.name}: {self.value}"
+        return f"{self.product.name} - {self.attribute_type}: {self.attribute_value}"
 
 
 class ProductReview(models.Model):
