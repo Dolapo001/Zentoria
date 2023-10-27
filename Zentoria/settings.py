84fd8c9fb23d4403a31146caf868ac90d524720e
Dolapo24 <adedolapovictoria927@gmail.com>
2023-10-27 08:58:37 +0100

    implemented jazzmin and swagger

--- Zentoria/settings.py
+++ Zentoria/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from .jazzmin import JAZZMIN_SETTINGS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,12 +33,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #thirdparty
+    'rest_framework',
+    'drf_yasg',
+
+    #apps
     'Products.apps.ProductsConfig',
 ]
 
@@ -123,3 +132,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+JAZZMIN_SETTINGS = JAZZMIN_SETTINGS
--- Zentoria/urls.py
+++ Zentoria/urls.py
@@ -15,8 +15,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view as swagger_get_schema_view
+
+schema_view = swagger_get_schema_view(
+    openapi.Info(
+        title="Zentoria_Api",
+        default_version="1.0.0",
+        description="API documentation of Zentoria",
+    ),
+    public=True
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/',
+         include([
+            path('api/products/', include(('Products.urls', 'Products'), namespace='products')),
+            path('swagger/schema/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-schema'),
+         ])
+         ),
 ]
