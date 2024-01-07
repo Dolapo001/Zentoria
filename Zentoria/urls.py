9dc66a05002c243149e5fa1414c1162724bd0477
Dolapo24 <adedolapovictoria927@gmail.com>
2024-01-07 21:29:33 +0100

    update

--- Zentoria/urls.py
+++ Zentoria/urls.py
@@ -17,19 +17,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-#urlpatterns_v1 = [
-    #path('accounts/', include('accounts.urls')),
-    #path("products/", include("Products.urls")),
-    #path("store/", include("store.urls")),
-#]
+urlpatterns_v1 = [
+    path('accounts/', include('accounts.urls')),
+    path("products/", include("Products.urls")),
+    path("store/", include("store.urls")),
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path("", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    path("api/v1/", include('accounts.urls')),
-    path("api/v1/", include('Products.urls')),
-    path("api/v1/", include('store.urls')),
+    path("api/v1/", include(urlpatterns_v1)),
+    #path("api/v1/", include('Products.urls')),
+    #path("api/v1/", include('store.urls')),
 ]
